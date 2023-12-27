e7b6a8e9f27d0d77831a1d5fd3ae64142e476de1
Semyon19988 <108398312+Semyon19988@users.noreply.github.com>
2023-12-27 17:08:24 +0300

    Integrated Ads in Project

---
+++ Assets/Sources/Ads/AdSettings.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Sources.Ads
+{
+	[CreateAssetMenu(fileName = "AdSettings", menuName = "ScriptableObjects/Ads/AdSettings")]
+	public class AdSettings : ScriptableObject
+	{
+		[SerializeField] private string _gameId;
+		[SerializeField] private bool _testMode;
+
+		public string GameId => _gameId;
+
+		public bool TestMode => _testMode;
+	}
+}
---
+++ Assets/Sources/View/UI/ExtraCoinsButton.cs
@@ -0,0 +1,44 @@
+﻿using Model.Currency;
+using Sources.Ads;
+using UnityEngine;
+using UnityEngine.Advertisements;
+using UnityEngine.UI;
+
+namespace Sources.View.UI
+{
+	[RequireComponent(typeof(Button))]
+	public class ExtraCoinsButton : MonoBehaviour, IUnityAdsShowListener
+	{
+		[SerializeField] private AdUnitIds _ids;
+		[SerializeField] private int _coinsForAd;
+
+		private Wallet _wallet;
+
+		public void Initialize(Wallet wallet)
+		{
+			_wallet = wallet;
+			var button = GetComponent<Button>();
+			button.onClick.AddListener(() => Advertisement.Show(_ids.Rewarded, this));
+		}
+		
+		public void OnUnityAdsShowFailure(string placementId, UnityAdsShowError error, string message)
+		{
+		}
+
+		public void OnUnityAdsShowStart(string placementId)
+		{
+		}
+
+		public void OnUnityAdsShowClick(string placementId)
+		{
+		}
+
+		public void OnUnityAdsShowComplete(string placementId, UnityAdsShowCompletionState showCompletionState)
+		{
+			if (showCompletionState != UnityAdsShowCompletionState.COMPLETED)
+				return;
+			
+			_wallet.Add(_coinsForAd);
+		}
+	}
+}
